31bfa4079e6af7bf509af4a8d29ae0afa1e09268
Frobski <37448330+Frobski@users.noreply.github.com>
2019-12-19 18:02:13 +0100

    Add files via upload

---
+++ server.lua
@@ -0,0 +1,11 @@
+ESX = nil
+
+TriggerEvent('esx:getSharedObject', function(obj) ESX = obj end)
+
+ESX.RegisterUsableItem('firebox', function(source)
+	local xPlayer = ESX.GetPlayerFromId(source)
+
+    xPlayer.removeInventoryItem('firebox', 1)
+    
+	TriggerClientEvent('frobski-fireworks:start', source)
+end)
